54f991d517d602a3fd9011cb852c2e7d10ffafc2
rgv176 <ragavimahenthiran@gmail.com>
2022-12-13 17:44:35 +0100

    feat: initialize get method

--- front/demo/src/components/read/read.js
+++ front/demo/src/components/read/read.js
@@ -1,48 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Table, Button } from "semantic-ui-react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export default function Read() {
-  const [apiData, setApiData] = useState([]);
   useEffect(() => {
-    axios.get("");
-  });
-  return (
-    <div>
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>ID</Table.HeaderCell>
-            <Table.HeaderCell>firstName</Table.HeaderCell>
-            <Table.HeaderCell>lastName</Table.HeaderCell>
-            <Table.HeaderCell>update</Table.HeaderCell>
-            <Table.HeaderCell>delete</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+    axios
+      .get("http://localhost:3001/read")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
 
-        <Table.Body>
-          {apiData.map((data) => {
-            return (
-              <Table.Row>
-                <Table.Cell>{data.id}</Table.Cell>
-                <Table.Cell>{data.firstName}</Table.Cell>
-                <Table.Cell>{data.lastName}</Table.Cell>
-                <Table.Cell>
-                  <Link to="/update">
-                    <button color="green">Update</button>
-                  </Link>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link to="/delete">
-                    <button color="red">Delete</button>
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
-    </div>
-  );
+  return <div>Read</div>;
 }
