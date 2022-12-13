bc1fb5d6a30948117b1e382ce262e6005ddaa92d
rgv176 <ragavimahenthiran@gmail.com>
2022-12-13 09:11:56 +0100

    feat: initialize front with first, last name

---
+++ front/demo/src/components/create/create.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+
+export default function Create() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  console.log(firstName);
+  console.log(lastName);
+  return (
+    <div>
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input
+            name="fname"
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="First Name"
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input
+            name="lname"
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Last Name"
+          />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
+}
