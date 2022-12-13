c54e99e25bdd17c0774ff9bd9628c97cd9babb78
rgv176 <ragavimahenthiran@gmail.com>
2022-12-13 10:48:08 +0100

    refactor: app page

--- front/demo/src/App.js
+++ front/demo/src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import Create from "./components/create/create";
+import Read from "./components/read/read";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Update from "./components/update/update";
+import Delete from "./components/delete/delete";
 
 function App() {
   return (
-    <div className="main">
-      <div>
-        <h3> Welcome !</h3>
+    <Router>
+      <div className="main">
+        <div>
+          <h3> Welcome !</h3>
+        </div>
+        <div>
+          <Route exact path="/" component={Create} />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Route exact path="read" component={Read} />
+        </div>
+        <Route path="update" component={Update} />
+        <Route path="delete" component={Delete} />
       </div>
-      <div>
-        <Create />
-      </div>
-    </div>
+    </Router>
   );
 }
 
