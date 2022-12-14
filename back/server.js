bc45a4e234d215bd86451816ecba597bad3a1195
rgv176 <ragavimahenthiran@gmail.com>
2022-12-14 12:17:10 +0100

    fix: correct bugs and unnecessary code

--- back/server.js
+++ back/server.js
@@ -3,6 +3,7 @@ var app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+mongoose.set("strictQuery", true);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
